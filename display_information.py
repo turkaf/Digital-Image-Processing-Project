20eb86e9cdd8d04da9cd2d8fddbd002f0d3430b2
Ahmet Faruk TÜRK <af_trk@hotmail.com>
2024-05-13 22:43:11 +0300

    display_information document has been updated by deleting MainPage object

--- display_information.py
+++ display_information.py
@@ -1,10 +1,8 @@
 from PyQt5.QtWidgets import *
 from display_information_python import Ui_Form
-from main_page import MainPage
 
 class DisplayInfoPage(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.displayForm = Ui_Form()
         self.displayForm.setupUi(self)
-        self.fetchInfos = MainPage()
