8f72c050b81f94b0af1b11303bd2aa557951f0b1
Ahmet Faruk TÜRK <af_trk@hotmail.com>
2024-05-14 20:17:16 +0300

    ObjectNames of widgets have been changed

--- main_page.py
+++ main_page.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
 from main_page_python import Ui_Form
 from display_information import DisplayInfoPage
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 from PIL import Image
 import numpy as np
 import os
@@ -13,6 +13,7 @@ class MainPage(QWidget):
         self.mainForm = Ui_Form()
         self.mainForm.setupUi(self)
         self.openDisplayInfoPage = DisplayInfoPage()
+        self.pixmap = QPixmap()
         self.mainForm.pushButton_select_image.clicked.connect(self.SelectImage)
         self.mainForm.pushButton_display_results.clicked.connect(self.DisplayInfo)
 
@@ -21,20 +22,22 @@ class MainPage(QWidget):
         file_dialog = QFileDialog()
         file_path, _ = file_dialog.getOpenFileName(self, 'Open Image', '.', 'Image Files (*.png *.jpg *.jpeg *.bmp)')
         image_name = os.path.split(file_path)[-1]
+
         if file_path:
-            print(file_path)
             self.mainForm.lbl_select_image.setText(image_name)
-            #self.pixmap = QPixmap(file_path)
 
             image = Image.open(file_path)
             numpy_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
             opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2RGB)
 
-            cv2.imshow("Image", opencv_image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            height, width, channel = opencv_image.shape
+            bytesPerLine = 3 * width
+            q_image = QImage(opencv_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+
+            self.pixmap = QPixmap.fromImage(q_image).scaled(width, height)
             
 
     def DisplayInfo(self):
         self.hide()
         self.openDisplayInfoPage.show()
+        self.openDisplayInfoPage.displayForm.label_original_image.setPixmap(self.pixmap)
